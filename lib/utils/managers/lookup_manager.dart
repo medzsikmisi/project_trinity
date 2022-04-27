3f9dbc000e58fdf3103392ad8772399efc09e82a
Csontos Mihály Dániel <csmisi000@gmail.com>
2022-04-27 18:10:49 +0200

    Updating hostname lookup

--- lib/utils/managers/lookup_manager.dart
+++ lib/utils/managers/lookup_manager.dart
@@ -1,10 +1,13 @@
 import 'package:get/get.dart';
 
 class LookupManager extends GetConnect {
+  LookupManager() {
+    baseUrl = 'http://localhost:8080/api/';
+  }
+
   Future<bool> lookUp(String hostname) async {
-    final result =
-        await post('http://localhost/api/lookup.php', {'hostname': hostname});
-    final isFree = result.body['free'];
+    final response = await get('check_domain.php', query: {'name': hostname});
+    final isFree = response.body?['available'];
     if (isFree == true) return true;
     return false;
   }
