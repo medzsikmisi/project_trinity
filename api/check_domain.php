04655c62a34a5a0aa17638843790494af73c382f
Farkas Erik <h044518@stud.u-szeged.hu>
2022-04-27 16:54:47 +0200

    check domainname

--- api/check_domain.php
+++ api/check_domain.php
@@ -1,11 +1,46 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+$returned = [];
+$tns = "
+(DESCRIPTION =
+    (ADDRESS_LIST =
+      (ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT = 1521))
+    )
+    (CONNECT_DATA =
+      (SID = orania2)
+    )
+  )";
+// Connect to database
+$conn = oci_connect("C##OFAX96", "C##OFAX96", $tns);
+//On error, send back an error
+if ($conn === false) {
+    $returned['success'] = false;
+    echo json_encode($returned);
+    return;
+}
 
-$returned =[
-    'success'=>'true',
-];
+$domainname = empty($_GET['name']) ? '' : $_GET['name'];
+$password = empty($_GET['password']) ? '' : $_GET['password'];
+
+if (empty($email) || empty($password)) {
+    $returned['success'] = false;
+    echo json_encode($returned);
+    oci_close($conn);
+    return;
+}
+$query = oci_parse($conn, "SELECT count(C##OFAX96.USERS.EMAIL) COUNT FROM C##OFAX96.DOMAINS WHERE C##OFAX96.DOMAINS.NAME = '$domainname'");
+oci_execute($query);
+$data = oci_fetch_assoc($query);
+$counter = $data["COUNT"];
+if ($counter == 1) {
+    $returned["success"] = true;
+    $returned["email"] = $email;
+} else {
+    $returned['success'] = false;
+}
+oci_close($conn);
 echo json_encode($returned);
